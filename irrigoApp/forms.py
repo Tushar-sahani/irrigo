51a254ca323ac470dffb04c46023a85572ff4256
Tushar Sahani <tusharsahani000@gmail.com>
2023-10-23 23:53:17 +0530

    validation

--- irrigoApp/forms.py
+++ irrigoApp/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 from .models import Detail
+
 class DetailForms(forms.ModelForm):
+
     class Meta:
         model = Detail
         fields = "__all__"
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['first_Name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_Name'].widget.attrs['class'] = 'form-control'
+        self.fields['phone_Number'].widget.attrs['class'] = 'form-control'
+        self.fields['adhar_Number'].widget.attrs['class'] = 'form-control'
+        self.fields['rfid'].widget.attrs['class'] = 'form-control'
+   
--- irrigoApp/views.py
+++ irrigoApp/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.db import IntegrityError
 from .models import Detail
+from .forms import DetailForms
 # Create your views here.
+# def index(request):
+#     if request.method == 'POST':
+#         first_Name = request.POST['first_Name']
+#         last_Name= request.POST['last_Name']
+#         phone_Number= request.POST['phone_Number']
+#         adhar_Number= request.POST['adhar_Number']
+#         rfid= request.POST['rfid']
+#         Detail.objects.create(first_Name=first_Name,last_Name=last_Name,phone_Number=phone_Number,adhar_Number=adhar_Number,rfid=rfid).save()
+#         messages.success(request,"Your Registration Successfull")
+
+#     return render(request,'index.html')
+
 def index(request):
     if request.method == 'POST':
-        first_Name = request.POST['first_Name']
-        last_Name= request.POST['last_Name']
-        phone_Number= request.POST['phone_Number']
-        adhar_Number= request.POST['adhar_Number']
-        rfid= request.POST['rfid']
-        Detail.objects.create(first_Name=first_Name,last_Name=last_Name,phone_Number=phone_Number,adhar_Number=adhar_Number,rfid=rfid).save()
-        messages.success(request,"Your Registration Successfull")
+        form = DetailForms(request.POST)
+        if form.is_valid():
+            adhar_number = form.cleaned_data['adhar_Number']
+            if Detail.objects.filter(adhar_Number=adhar_number).exists():
+                print("yes THE DATA IS EXIT THER IN DATABASE")
+                messages.error(request,"User already in database")
+            else:
+                form.save()
+                messages.success(request,"Your Response is Subimited")
+    else:
+        form = DetailForms() 
 
-    return render(request,'index.html') 
+    return render(request, 'index.html', {'forms': form})
