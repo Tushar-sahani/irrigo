cfb4040462d96d22208720534e137933e09cfb04
Tushar Sahani <tusharsahani000@gmail.com>
2023-10-29 10:14:56 +0530

    vercel settings

--- irrigo/settings.py
+++ irrigo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^)q3%*@shs(mo(_gci)$in*yino!)z(uinb@wqxlj)cn=19f99
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
