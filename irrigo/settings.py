3dfbd59f20d700dc4b905d7e557ac3e0afbc6004
Tushar Sahani <tusharsahani000@gmail.com>
2023-10-29 10:32:00 +0530

    staticfiles

--- irrigo/settings.py
+++ irrigo/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'irrigoApp/static'),]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
