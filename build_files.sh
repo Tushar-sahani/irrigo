e2f65923f55831a6155d6334f0da48289d985ad0
Tushar Sahani <tusharsahani000@gmail.com>
2023-10-29 12:49:21 +0530

    setting

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
-echo "BUILD START"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+# build_files.sh
+python -m pip install -r requirements.txt
+python3.9 manage.py collectstatic 
