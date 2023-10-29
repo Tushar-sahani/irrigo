1efdd3f9a4a3357bb297563315b5e1f251af1ef5
Tushar Sahani <tusharsahani000@gmail.com>
2023-10-29 11:04:34 +0530

    staticfiles

--- irrigo/settings.py
+++ irrigo/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'irrigo/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'irrigo/static'),
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build')
 # Default primary key field type
